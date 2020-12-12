a8fd064d3a6f8d49eb01b7cf6a1f9b00b922c194
0xLeif <zmeriksen@icloud.com>
2020-12-12 10:07:41 -0600

    Initial Commit

---
+++ SwiftTyping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTyping
+//
+//  Created by Zach Eriksen on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
