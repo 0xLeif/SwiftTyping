8168a7bc5fcb8ea22815b300e08f8afc07f3639d
0xLeif <zmeriksen@icloud.com>
2020-12-12 10:23:03 -0600

    init

--- SwiftTyping/ViewController.swift
+++ SwiftTyping/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import SwiftUIKit
+import Fake
 
 class ViewController: UIViewController {
+    var word = Fake.Word.Random.sentence(words: 5)
+    var typed = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Navigate.shared.configure(controller: navigationController)
+     
+        
+        draw()
     }
-
-
+    
+    private func draw() {
+        view.clear().embed(withPadding: 32) {
+            VStack {
+                [
+                
+                    Label(word),
+                    
+                    Field(value: "", placeholder: "...", keyboardType: .default)
+                        .inputHandler { [weak self] in self?.typed = $0 },
+                    
+                    Button("Done") { [weak self] in
+                        self?.check()
+                    }
+                    
+                ]
+            }
+        }
+    }
+    
+    private func check() {
+        defer {
+            word = Fake.Word.Random.sentence(words: 5)
+            typed = ""
+            draw()
+        }
+        
+        guard word == typed else {
+            Navigate.shared.alert(title: "Wrong!", message: "", secondsToPersist: 3)
+            return
+        }
+        
+        Navigate.shared.alert(title: "Correct!", message: "", secondsToPersist: 3)
+    }
 }
 
